a14091343e003906d4862f23c28f7aed85a39538
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-07-09 14:36:01 -0300

    Ammend commit - 09-07-2024 14h35

--- src/database/knex/migrations/20240421160445_createOrders.js
+++ src/database/knex/migrations/20240421160445_createOrders.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("orders", t => {
   t.increments("order_id");
   t.integer('user_id').notNullable();
   
-  t.enum('payment_method', ['credito', 'pix'], { useNative: true, enumName: "payment_methods" })
+  t.enum('payment_method', ['pix', 'credito', 'debito'], { useNative: true, enumName: "payment_methods" })
     .notNullable().default('crédito');
 
   t.enum('status', ['pendente', 'preparando', 'entregue', 'cancelado'], { useNative: true, enumName: "status" })
