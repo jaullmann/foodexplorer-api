cc00b550ac7526d28f18f1f94d2913c0a002aca6
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-09-01 19:41:42 -0300

    Amend commit

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173", 
     "http://127.0.0.1:5173/",
-    "https://foodexplorer2024.netlify.app/"
+    "https://foodexplorer2024.netlify.app"
   ],
   credentials: true
 }));    
